498406190cf1456d80090cb7b4e5de0b05dff53d
galang8664 <95033122+galang8664@users.noreply.github.com>
2023-04-03 14:45:14 +0700

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://gitlab.com/Project-Madhav/nad/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
-git clone https://github.com/kitw4y/local_manifest.git --depth 1 -b nad .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/Octavi-Staging/manifest.git -b thirteen -g default,-mips,-darwin,-notdefault
+git clone https://github.com/GhostMaster69-dev/local_manifest.git --depth 1 -b thirteen .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh 
-lunch nad_lancelot-userdebug
-export TZ=Asia/Jakarta #put before last build command
-make nad
+source build/envsetup.sh
+lunch octavi_vince-user
+#export USE_GAPPS=true
+#export WITH_GAPPS=true
+mka bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
