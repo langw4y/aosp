3ba5e4ac370f376dc196ec69137712f49c2f163f
galang8664 <95033122+galang8664@users.noreply.github.com>
2023-04-03 12:07:36 +0700

    cipher

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/kitw4y/local_manifest.git --depth 1 -b cipher .repo
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+source build/envsetup.sh 
 lunch cipher_lancelot-userdebug
 export TZ=Asia/Jakarta #put before last build command
 mka bacon
